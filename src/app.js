808ebd56db4598a5fef66ded9f257b89db8e0ef7
bvv94 <bvdevasconcelos@gmail.com>
2023-04-12 17:24:44 -0400

    Configurações dotenv OK

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 
 //---Criação do Servidor---//
@@ -8,15 +9,16 @@ const app = express();
 const PORT = 5000;
 
 //---App escutando à espera de requisições---//
-app.listen (PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
 
 //--Configurações--//
 app.use(express.json());
-app.use (cors());
+app.use(cors());
+dotenv.config();
 
 //---Configurações Banco de Dados---//
 let db;
-let MongoClient = new MongoClient("mongodb://localhost:27017/batepapouol")
+let MongoClient = new MongoClient(process.env.DATABASE_URL)
 MongoClient.connect()
-    .then()
-    .catch()
+    .then(() => db = MongoClient.db())
+    .catch((err) => console.log(err.message))
