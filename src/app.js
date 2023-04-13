cc193d6a71105e37c15f3e7d74f0ae1ffb7c97bd
bvv94 <bvdevasconcelos@gmail.com>
2023-04-13 10:00:46 -0400

    Endpoints iniciados

--- src/app.js
+++ src/app.js
@@ -2,14 +2,11 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-
+import dayjs from "dayjs";
 
 //---Criação do Servidor---//
 const app = express();
 const PORT = 5000;
-
-//---App escutando à espera de requisições---//
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
 
 //--Configurações--//
 app.use(express.json());
@@ -22,3 +19,64 @@ let MongoClient = new MongoClient(process.env.DATABASE_URL)
 MongoClient.connect()
     .then(() => db = MongoClient.db())
     .catch((err) => console.log(err.message))
+
+//---Endpoints---//
+app.post("/participants", (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(422).send("Todos os campos são obrigatórios!")
+    }
+    // Validações com a Joi
+    // Impedir cadastro repetido
+
+    const newName = { name, lastStatus: Date.now() };
+    db.collection("participants").insertOne(newName);
+
+    const newMsg = { from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:mm:ss' }
+    db.collection("messages").insertOne(newMsg);
+
+})
+
+app.get("/participants", (req, res) => {
+    db.collection("participants").find().toArray()
+        .then((participants) => res.status().send(participants))
+        .catch((err) => res.status(500).send(err.message)) //---mandar array vazio aqui?---//
+})
+
+app.post("/messages", (req, res) => {
+    const { to, text, type } = req.body
+    //---pegar Header---//
+
+    const newMsg = {
+        // from: User, pegar do Header
+        to,
+        text,
+        type,
+        time: 'HH:mm:ss' // dayjs
+    }
+    if ((to || text !== '') || (type !== 'message') || (type !== 'private_message')) {
+        //if () { Validação do from como participante existente
+            db.collection("messages").insertOne(newMsg)
+                .then(res.status(201)
+                    )
+                .catch(res.status(422))
+      //}
+    }
+})
+
+// voltar a codar daqui app.get("/messages", (req, res) => {})
+
+
+
+
+
+
+
+
+
+
+
+
+//---App escutando à espera de requisições---//
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
