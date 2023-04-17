7dd734f5724fcd635ae6e246ae2a4c045cefe79b
bvv94 <bvdevasconcelos@gmail.com>
2023-04-17 07:52:47 -0400

    get Messages corrigido

--- src/app.js
+++ src/app.js
@@ -105,8 +105,8 @@ app.get("/messages", async (req, res) => {
         const messages = await db.collection('messages').find({
             $or: [
                 { from: 'Todos' },
-                { to: user },
-                { from: user }
+                { to: User },
+                { from: User }
             ]
         }).toArray();
 
